5b5ab6ad36461fc409e051c92b9661c31c996d6a
ImGreeny <imtherealgreeny@gmail.com>
2020-10-26 08:40:59 +0100

    Added listeners and GameManager getter

--- Spigot/src/main/java/pw/greeny/manhuntextended/ManhuntExtended.java
+++ Spigot/src/main/java/pw/greeny/manhuntextended/ManhuntExtended.java
@@ -3,6 +3,9 @@ package pw.greeny.manhuntextended;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import pw.greeny.manhuntextended.core.managers.GameManager;
+import pw.greeny.manhuntextended.listener.ConfigListener;
+import pw.greeny.manhuntextended.listener.LobbyListener;
+import pw.greeny.manhuntextended.listener.MainListener;
 
 import java.util.logging.Level;
 
@@ -19,6 +22,10 @@ public final class ManhuntExtended extends JavaPlugin {
 
         gameManager = new GameManager(this);
 
+        Bukkit.getPluginManager().registerEvents(new MainListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new LobbyListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ConfigListener(this), this);
+
         Bukkit.getLogger().log(Level.INFO, "ManhuntExtended loaded!");
     }
 
@@ -29,5 +36,9 @@ public final class ManhuntExtended extends JavaPlugin {
 
     public ManhuntExtended getInstance() {
         return instance;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
     }
 }
